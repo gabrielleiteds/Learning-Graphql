c3ca7f303dff21ecf841493363ed661acb47363e
leitegs <leitedasilvag@gmail.com>
2020-06-22 21:41:20 -0300

    function of createUser finished

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
-import { GraphQLServer } from 'graphql-yoga';
+const { GraphQLServer } = require('graphql-yoga');
+const mongoose = require('mongoose')
+const path = require('path')
 
-const server = new GraphQLServer({})
+const resolvers = require('./resolvers')
 
-server.start; 
+require('./connect')
+
+const server = new GraphQLServer({
+    typeDefs: path.resolve(__dirname, 'schema.graphql'),
+    resolvers
+})
+
+server.start(3000); 
